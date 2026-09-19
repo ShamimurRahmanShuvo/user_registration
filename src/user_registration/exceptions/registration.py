5d7a314f0315bc67b7467df6b15b8a18d415ddfd
ShamimurRahmanShuvo <shamimur.shuvo@gmail.com>
2026-09-19 09:25:35 -0400

    Add registration workflow with custom errors

--- src/user_registration/exceptions/registration.py
+++ src/user_registration/exceptions/registration.py
@@ -1,3 +1,16 @@
 """
 Registration exception hierarchy foundation.
 """
+from __future__ import annotations
+
+
+class RegistrationError(Exception):
+    """Base exception for registration-related errors"""
+
+
+class UserAlreadyExistsError(RegistrationError):
+    """Raised when a username or email is already registered"""
+
+
+class UserNotFoundError(RegistrationError):
+    """Raised when an expected user cannot be found"""
